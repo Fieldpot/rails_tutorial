6c6fc82d8d3bf72d858c8d13dd71ae188e5b07d2
Fieldpot <fieldpotsec@gmail.com>
2019-02-12 19:35:50 +0900

    Add hello method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def goodbye
-    render html: "goodbye"
+  def hello
+    render html: "hello, world!"
   end
 end
